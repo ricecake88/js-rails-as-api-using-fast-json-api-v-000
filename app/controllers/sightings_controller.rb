4014db0e0e4c3c3ff9c50e0e530d7c7640d67229
Grace Shih <vocalise@gmail.com>
2020-01-20 01:46:31 +0000

    Automatically backed up by Learn

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -1,6 +1,5 @@
 class SightingsController < ApplicationController
   def show
     @sighting = Sighting.find(params[:id])
-    render json: @sighting.to_json(:include => {:bird => {:only =>[:name, :species]}, :location => {:only =>[:latitude, :longitude]}}, :except => [:updated_at])
-  end
+    render json: SightingSerializer.new(sighting)
 end
